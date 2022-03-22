c609d3724c11afea759fb3b12a50512fb8d7620c
Mikhail Murunov <haskiindahouse@gmail.com>
2022-03-22 18:41:55 +0300

    modify system

--- Parser.cpp
+++ Parser.cpp
@@ -1,19 +1,72 @@
 //
 // Created by Михаил Мурунов on 15.03.2022.
 //
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <cctype>
+#include <algorithm>
+#include <set>
+#include <vector>
 
 #include "Parser.h"
+#include "Expression.h"
+// на вход просто строка ->
+// бьем ее на правила через запятую
+// обязательно находим старт для правил
+// S->AB, A->0, B->1
+// set<Expression> = {Expression(S->AB), Expression(A->0), Expression(B->1)}
+// причем нам известно что S => root-Expression.
+// Далее нужен Exprerssion executor.
+// Будем иметь точку входа. И далее по контейнеру идти и выбирать.
+Parser::Parser(const std::string& s) :
+m_originalIns(s),
+m_rules(),
+m_state(Actions::ParseState::Begin)
+{
+    auto preRules = tokenize(s);
+    for (const auto& rule : preRules)
+    {
+        m_rules.insert(new Expression(rule));
+    }
+}
 
-Parser::Parser() :
-m_rules(std::set<std::string>())
-{}
-
-std::set<std::string> Parser::rules()
+std::set<Expression*> Parser::rules()
 {
     return m_rules;
 }
 
-void Parser::addRule(const std::string* newRule)
+void Parser::addRule(const std::string newRule)
 {
-    m_rules.insert(newRule->data());
+
+    //if()
+    /*
+     * Нужно решить как будут выглядеть
+     *
+     */
+
+}
+
+std::string Parser::originalInput() {
+    return m_originalIns;
+}
+
+std::set<std::string> Parser::tokenize(std::string s, std::string del)
+{
+    std::set<std::string> ret;
+
+    int start = 0;
+    int end = s.find(del);
+    while (-1 != end)
+    {
+        auto value = s.substr(start, end);
+
+        ret.insert(trim(value));
+        s.erase(start, end + del.size());
+        end = s.find(del, start);
+    }
+
+    ret.insert(trim(s));
+
+    return ret;
 }
